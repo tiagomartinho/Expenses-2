c4e0bff67792fed2dd355ad58c52189f4cacf6c6
Tiago Martinho <tiago.martinho@xpeppers.com>
2015-07-04 01:13:23 +0200

    add support to link a dropbox account to the app removed auto generated code and default launch screen

--- Expenses:2/ViewController.swift
+++ Expenses:2/ViewController.swift
@@ -1,25 +1,10 @@
-//
-//  ViewController.swift
-//  Expenses:2
-//
-//  Created by Martinho on 03/07/15.
-//  Copyright (c) 2015 TM. All rights reserved.
-//
-
 import UIKit
+import SwiftyDropbox
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    @IBAction func linkDropbox(sender: UIBarButtonItem) {
+        Dropbox.authorizeFromController(self)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
