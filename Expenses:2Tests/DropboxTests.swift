cbe89c82821899ac8d50b5bcb21522a7884db1e4
Tiago Martinho <tiago.martinho@xpeppers.com>
2015-07-05 20:42:27 +0200

    add some more tests to dropbox API

--- Expenses:2Tests/DropboxTests.swift
+++ Expenses:2Tests/DropboxTests.swift
@@ -21,4 +21,77 @@ class DropboxTests: XCTestCase {
         waitForExpectationsWithTimeout(10) { (error) in
         }
     }
+    
+    func testListFolderContents(){
+        let expectation = expectationWithDescription(nil)
+
+        client!.filesListFolder(path: "").response { response, error in
+            if let result = response {
+                for entry in result.entries {
+                    println(entry.name)
+                }
+                expectation.fulfill()
+            } else {
+                println(error!)
+            }
+        }
+        
+        waitForExpectationsWithTimeout(10) { (error) in
+        }
+    }
+    
+    func testUploadFile(){
+        let expectation = expectationWithDescription(nil)
+
+        let fileName = "hello.txt"
+        let fileData = "Hello!".dataUsingEncoding(NSUTF8StringEncoding, allowLossyConversion: false)
+        
+        client!.filesUpload(path: "/" + fileName, body: fileData!).response { response, error in
+            if let metadata = response {
+                expectation.fulfill()
+            } else {
+                println(error!)
+            }
+        }
+        
+        waitForExpectationsWithTimeout(10) { (error) in
+        }
+    }
+    
+    func testDownloadFile(){
+        let expectation = expectationWithDescription(nil)
+        
+        client!.filesDownload(path: "/hello.txt").response { response, error in
+            if let (metadata, data) = response {
+                expectation.fulfill()
+            } else {
+                println(error!)
+            }
+        }
+        
+        waitForExpectationsWithTimeout(10) { (error) in
+        }
+    }
+
+    func testGetFileMetadata(){
+        let expectation = expectationWithDescription(nil)
+
+        client!.filesGetMetadata(path: "/hello.txt").response { response, error in
+            if let metadata = response {
+                println("Name: \(metadata.name)")
+                if let file = metadata as? Files.FileMetadata {
+                    println("This is a file.")
+                    println("File size: \(file.size)")
+                    expectation.fulfill()
+                } else if let folder = metadata as? Files.FolderMetadata {
+                    println("This is a folder.")
+                }
+            } else {
+                println(error!)
+            }
+        }
+        
+        waitForExpectationsWithTimeout(10) { (error) in
+        }
+    }
 }
--- Podfile
+++ Podfile
@@ -4,3 +4,4 @@ use_frameworks!
 link_with 'Expenses:2','Expenses:2Tests'
 
 pod 'SwiftyDropbox', :git => 'git@github.com:dropbox/SwiftyDropbox.git', :tag => '0.2'
+pod 'RealmSwift'
