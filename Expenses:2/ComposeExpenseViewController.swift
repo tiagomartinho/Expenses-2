8032ac5394a50801fdd9d288cfeb6a205f4f9fe1
Tiago Martinho <tiago.martinho@xpeppers.com>
2015-07-06 19:46:28 +0200

    finished defining ui for compose expense view  extend expense object to contain also a person and category

--- Expenses:2/ComposeExpenseViewController.swift
+++ Expenses:2/ComposeExpenseViewController.swift
@@ -1,35 +1,29 @@
-//
-//  ComposeExpenseViewController.swift
-//  Expenses:2
-//
-//  Created by Martinho on 05/07/15.
-//  Copyright (c) 2015 TM. All rights reserved.
-//
-
 import UIKit
 
 class ComposeExpenseViewController: UIViewController {
 
+    @IBOutlet weak var person: UISegmentedControl!
+    @IBOutlet weak var amount: UITextField!
+    @IBOutlet weak var category: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        amount.becomeFirstResponder()
     }
     
+    @IBAction func save(sender: UIBarButtonItem) {
+        if let person = person.titleForSegmentAtIndex(person.selectedSegmentIndex) {
+            
+        }
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        dismissViewController()
     }
-    */
-
+    
+    @IBAction func cancel(sender: UIBarButtonItem) {
+        dismissViewController()
+    }
+    
+    func dismissViewController(){
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
 }
--- Expenses:2/Expense.swift
+++ Expenses:2/Expense.swift
@@ -2,5 +2,7 @@ import RealmSwift
 
 class Expense: Object {
     dynamic var amount = 0
+    dynamic var category = ""
+    dynamic var person = ""
     dynamic var date = NSDate()
 }
