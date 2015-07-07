9e905afb1346fb576128ca2f3264201e0ae3eb0e
Tiago Martinho <tiago.martinho@xpeppers.com>
2015-07-07 22:22:42 +0200

    save persons names in defaults

---
+++ Constants.swift
@@ -0,0 +1,6 @@
+import Foundation
+
+// User Defaults
+let defaults = NSUserDefaults.standardUserDefaults()
+let kUD_Person1: String = "kUD_Person1"
+let kUD_Person2: String = "kUD_Person2"
--- Expenses/SettingsViewController.swift
+++ Expenses/SettingsViewController.swift
@@ -1,4 +1,37 @@
 import UIKit
 
 class SettingsViewController: UITableViewController {
+    
+    @IBOutlet weak var person1: UITextField!
+    @IBOutlet weak var person2: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        loadPersonsNames()
+    }
+    
+    func loadPersonsNames(){
+        if let person1Name = defaults.objectForKey(kUD_Person1) as? String {
+            person1.text = person1Name
+        }
+        if let person2Name = defaults.objectForKey(kUD_Person2) as? String {
+            person2.text = person2Name
+        }
+    }
+    
+    @IBAction func person(sender: UITextField) {
+        let name = sender.text.removeWhitespaces()
+        if name == "" {
+            return
+        }
+        
+        if sender.placeholder?.hasSuffix("1") ?? false {
+            defaults.setObject(name, forKey: kUD_Person1)
+        }
+        
+        if sender.placeholder?.hasSuffix("2") ?? false {
+            defaults.setObject(name, forKey: kUD_Person2)
+        }
+    }
 }
