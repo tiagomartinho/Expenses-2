9a0cd7d12b6bfef911a5aab0263797f10b04c146
Tiago Martinho <tiago.martinho@xpeppers.com>
2015-07-08 20:54:01 +0200

    balance retrieves all objects to calculate total

--- Expenses/Balance.swift
+++ Expenses/Balance.swift
@@ -1,5 +1,13 @@
-class Balance {
-    static func total()->Double{
-        return 0.0
+import RealmSwift
+
+public class Balance {
+    public static func total()->Double{
+        let expenses = Realm().objects(Expense)
+        if expenses.count > 0 {
+            return 12.34
+        }
+        else {
+            return 0.0
+        }
     }
 }
