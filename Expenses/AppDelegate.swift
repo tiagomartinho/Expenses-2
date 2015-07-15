fa9e2e7e6a4ac657134680263ea2f5170ace25b8
Tiago Martinho <tiago.martinho@xpeppers.com>
2015-07-15 20:19:31 +0200

    save iCloud token

--- Expenses/AppDelegate.swift
+++ Expenses/AppDelegate.swift
@@ -3,5 +3,27 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
+    
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject : AnyObject]?) -> Bool {
+        iCloudAvailability()
+        return true
+    }
+    
+    var listenningForUbiquityChanges = false
+    func iCloudAvailability(){
+        if !listenningForUbiquityChanges {
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "iCloudAvailability:", name: NSUbiquityIdentityDidChangeNotification, object: nil)
+            listenningForUbiquityChanges = true
+        }
+
+        let fileManager = NSFileManager.defaultManager()
+        if let currentiCloudToken = fileManager.ubiquityIdentityToken {
+            let newTokenData = NSKeyedArchiver.archivedDataWithRootObject(currentiCloudToken)
+            k.Defaults.setObject(newTokenData, forKey: k.UD_IdentityToken)
+        }
+            else {
+            k.Defaults.removeObjectForKey(k.UD_IdentityToken)
+        }
+    }
 }
 
--- Expenses/Constants.swift
+++ Expenses/Constants.swift
@@ -3,8 +3,9 @@ import Foundation
 struct k {
     // User Defaults
     static let Defaults = NSUserDefaults.standardUserDefaults()
-    static let UD_Person1: String = "kUD_Person1"
-    static let UD_Person2: String = "kUD_Person2"
+    static let UD_Person1 = "kUD_Person1"
+    static let UD_Person2 = "kUD_Person2"
+    static let UD_IdentityToken = "com.apple.MyAppName.UbiquityIdentityToken"
     static var Person1Name:String { return Defaults.objectForKey(UD_Person1) as? String ?? "default_person_1_name".localized }
     static var Person2Name:String { return Defaults.objectForKey(UD_Person2) as? String ?? "default_person_2_name".localized }
     
