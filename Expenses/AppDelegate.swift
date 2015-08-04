1bab3132e676966315822c29a68bb185fbd3166e
Tiago Martinho <tiago.martinho@xpeppers.com>
2015-08-05 00:23:41 +0200

    overwrite realm file remove imported temp file

--- Expenses/AppDelegate.swift
+++ Expenses/AppDelegate.swift
@@ -1,10 +1,31 @@
 import UIKit
+import RealmSwift
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     
-    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject : AnyObject]?) -> Bool {
+    func application(application: UIApplication, handleOpenURL url: NSURL) -> Bool
+    {
+        overwrite(url)
+        removeImportedFile(url)
         return true
+    }
+    
+    private func overwrite(url: NSURL){        
+        let defaultPath = Realm().path
+        let defaultParentPath = defaultPath.stringByDeletingLastPathComponent
+        
+        if let path = url.path {
+            NSFileManager.defaultManager().removeItemAtPath(defaultPath, error: nil)
+            NSFileManager.defaultManager().copyItemAtPath(path, toPath: defaultPath, error: nil)
+        }        
+    }
+    
+    private func removeImportedFile(url: NSURL){
+        let fileManager = NSFileManager()
+        if let path = url.path {
+            fileManager.removeItemAtPath(path, error: nil)
+        }
     }
 }
